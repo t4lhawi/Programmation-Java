f6a15e0e191dfb16daad26b20918556fee517c16
Mohamed Talhaoui <144726758+t4lhawi@users.noreply.github.com>
2025-12-10 21:30:56 +0100

    fix ex1 tp4

--- TP4/EX1.java
+++ TP4/EX1.java
@@ -3,89 +3,81 @@
 import java.util.*;
 
 public class EX1 {
-    static int[][] moyenneur4(int[][] I){
-        int h = I.length;
-        int l = I[0].length;
-        
-        int[][] R = new int[h][l];
 
-        for (int a = 0; a < h; a++) {
-            for (int b = 0; b < l; b++) {
-                int somme = I[a][b];
-                int n = 1;
-
-                if (a > 0) { 
-                    somme += I[a-1][b]; 
-                    n++; 
+    static void elementPetitGrand(int[] T){
+        int N = T.length;
+        for(int i=0; i<N; i++){
+            int minIdx = i;
+            for(int j=i + 1; j <N; j++){
+                if(T[j] > T[minIdx]){ // pour decroisante on utilise <
+                    minIdx = j;
                 }
-                if (a < h-1) { 
-                    somme += I[a+1][b]; 
-                    n++; 
-                }
-                if (b > 0) { 
-                    somme += I[a][b-1]; 
-                    n++; 
-                }
-                if (b < l-1) { 
-                    somme += I[a][b+1]; 
-                    n++; 
-                }
-
-                R[a][b] = somme / n;
+                int tmp = T[minIdx];
+                T[minIdx] = T[i];
+                T[i] = tmp;
             }
         }
-        return R;
+        System.out.println("Plus Grand est : " + T[N-1]);
+        System.out.println("Plus Petit est : " + T[0]);
     }
     
-    static int[][] moyenneur8(int[][] I) {
-        int h = I.length, l = I[0].length;
-        int[][] R = new int[h][l];
-
-        for (int a = 0; a < h; a++) {
-            for (int b = 0; b < l; b++) {
-
-                int somme = 0;
-                int n = 0;
-
-                for (int i = -1; i <= 1; i++) {
-                    for (int j = -1; j <= 1; j++) {
-                        int x = a + i;
-                        int y = b + j;
-                        if (x >= 0 && x < h && y >= 0 && y < l) {
-                            somme += I[x][y];
-                            n++;
-                        }
-                    }
+    static int[] inserer(int[] T, int x){
+        int N = T.length + 1;
+        T = new int[N];
+        T[N-1] = x;
+        for(int i=0; i<N; i++){
+            int minIdx = i;
+            for(int j=i + 1; j <N; j++){
+                if(T[j] > T[minIdx]){ // pour decroisante on utilise <
+                    minIdx = j;
                 }
-                R[a][b] = somme / n;
+                int tmp = T[minIdx];
+                T[minIdx] = T[i];
+                T[i] = tmp;
             }
         }
-        return R;
+        return T;
     }
     
-    static void afficher(int[][] M) {
-        for (int[] row : M) {
-            for (int v : row) System.out.print(v + " ");
-                System.out.println();
+    
+    static void apparition(int[] T, int x){
+        int a = 0;
+        for(int i=0; i<T.length; i++){
+            if(T[i] == x){
+                a++;
             }
         }
+        System.out.println(x + " apparaît " + a + "fois");
+    }
     
+    static void afficher(int[] T){
+        System.out.print("[");
+        for(int i=0; i<T.length; i++){
+            System.out.print(T[i] + ", ");
+        }
+        System.out.println("]");
+    }
     
     public static void main(String[] args) {
-
-    int[][] I = {
-        {10, 20, 30},
-        {40, 50, 60},
-        {70, 80, 90}
-    };
-
-    int[][] F4 = moyenneur4(I);
-    System.out.println("Filtre Connexion 4 :");
-    afficher(F4);
-    
-    
-    int[][] F8 = moyenneur8(I);
-    System.out.println("Filtre Connexion 8 :");
-    afficher(F8);
+        int[] T = {4, 8, 9, 7, 6};
+        
+        for(int i=0; i<5; i++){
+            int minIdx = i;
+            for(int j=i + 1; j <5; j++){
+                if(T[j] < T[minIdx]){ // pour decroisante on utilise >
+                    minIdx = j;
+                }
+                int tmp = T[minIdx];
+                T[minIdx] = T[j];
+                T[j] = tmp;
+            }
+        }
+        
+        afficher(T);
+        elementPetitGrand(T);
+       
+        
+        T = inserer(T, 5);
+        afficher(T);
     }
 }
